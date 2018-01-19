52e25c263cd8dfe6b4b661a8879c0bb3191bc6b2
piggsoft <piggsoft@163.com>
2018-01-19 13:25:59 +0800

    去除tomcat 改成undertow，

---
+++ src/main/java/com/piggsoft/blog/controller/IndexController.java
@@ -0,0 +1,20 @@
+/*
+ *
+ * Copyright (C) 1999-2016 IFLYTEK Inc.All Rights Reserved.
+ * History：
+ * Version   Author      Date                              Operation
+ * 1.0       yaochen4    2018/1/19                           Create
+ */
+package com.piggsoft.blog.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author piggsoft
+ * @version 1.0
+ * @create 2018/1/19
+ * @since 1.0
+ */
+@Controller
+public class IndexController {
+}
